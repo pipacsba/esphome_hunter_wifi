9d12bd5e341d2d75af53128c27ee36511e5e45bb
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-04-03 11:34:06 +0200

    Created based on ESPHome Dallas component

---
+++ components/hunter_wifi/__init__.py
@@ -0,0 +1,25 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.const import CONF_ID, CONF_PIN
+
+MULTI_CONF = True
+AUTO_LOAD = ["switch"]
+
+dallas_ns = cg.esphome_ns.namespace("hunter-wifi")
+HunterWifiComponent = dallas_ns.class_("HunterWifiComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(HunterWifiComponent),
+        cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
