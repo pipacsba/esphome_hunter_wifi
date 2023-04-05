d1c3e8d3836a8f53b59ef56c9e66a53287305213
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-04-05 12:25:39 +0200

    Update hunter_wifi.h

--- components/hunter_wifi/hunter_wifi.h
+++ components/hunter_wifi/hunter_wifi.h
@@ -2,6 +2,8 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/switch/switch.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/number/number.h"
 #include "HunterRoam.h"
 
 #include <vector>
@@ -9,7 +11,8 @@
 namespace esphome {
 namespace hunterwifi {
 
-class HunterZoneSwitch;
+class HunterWifiComponent;      // this component
+class HunterZoneSwitch;         // switches representing any valve / zones
 
 class HunterWifiComponent  : public Component {
  public:
@@ -18,11 +21,22 @@ class HunterWifiComponent  : public Component {
 
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::DATA; }
+  void set_name(const std::string &name) { this->name_ = name; }
+  void add_valve(HunterZoneSwitch *valve_sw); 
 
-  void update() override;
-
+  /// returns a pointer to a valve's switch object
+  HunterZoneSwitch *valve_switch(size_t valve_number);
+ 
  protected:
+   /// returns true if valve number is enabled
+  bool valve_is_enabled_(size_t valve_number);
+ 
+  /// returns true if any valve is enabled
+  bool any_valve_is_enabled_();
+ 
+  /// Sprinkler valve objects
+  std::vector<HunterZoneSwitch> valve_;
+ 
   friend HunterZoneSwitch;
 
   InternalGPIOPin *pin_;
@@ -30,50 +44,29 @@ class HunterWifiComponent  : public Component {
 
 };
 
-/// Internal class that helps us create multiple sensors for one Dallas hub.
-class HunterZoneSwitch : public switch::Switch {
+class HunterZoneSwitch {
  public:
-  void set_parent(HunterWifiComponent *parent) { parent_ = parent; }
-  /// Helper to get a pointer to the address as uint8_t.
-  /// uint8_t *get_address8();  - ez a Dallas sensor cime vaj'h?
-  /// Helper to create (and cache) the name for this switch. For example "Front Garden".
-  const std::string &get_zone_name();
+  HunterZoneSwitch();
+  HunterZoneSwitch(switch_::Switch *hunterwifi_switch);
+  HunterZoneSwitch(switch_::Switch *off_switch, switch_::Switch *on_switch);
 
-  /// Get the index of this sensor.
-  void uint8_t get_index() const;
-  /// Set the index of this sensor.
-  void set_index(uint8_t index);
-
-  bool setup_switch();
-
-  int get_result();
-
-  std::string unique_id() override;
+  bool state();  // returns the switch's current state
+  void set_off_switch(switch_::Switch *off_switch) { this->off_switch_ = off_switch; }
+  void set_on_switch(switch_::Switch *on_switch) { this->on_switch_ = on_switch; }
+  void sync_valve_state();  // syncs internal state to switch;
+  void turn_off();        // sets internal flag and actuates the switch
+  void turn_on();         // sets internal flag and actuates the switch
+  switch_::Switch *off_switch() { return this->off_switch_; }
+  switch_::Switch *on_switch() { return this->on_switch_; }
 
  protected:
-  HunterWifiComponent *parent_;
-  uint8_t index_;
-
-  std::string zone_name_;
+  bool state_{false};
+  uint32_t zone_number_{0};
+  //switch_::Switch *off_switch_{nullptr};  // only used for latching valves
+  switch_::Switch *on_switch_{nullptr};   // used for both latching and non-latching valves
 };
  
- #include "esphome.h"
 
-class MyCustomSwitch : public Component, public Switch {
- public:
-  void setup() override {
-    // This will be called by App.setup()
-    pinMode(5, INPUT);
-  }
-  void write_state(bool state) override {
-    // This will be called every time the user requests a state change.
-
-    digitalWrite(5, state);
-
-    // Acknowledge new state by publishing it
-    publish_state(state);
-  }
-};
-
-}  // namespace dallas
+ 
+}  // namespace hunterwifi
 }  // namespace esphome
