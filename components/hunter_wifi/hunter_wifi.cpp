e7a6349b88004f1814cac85c73e5a76b41c27c7a
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-04-06 11:03:40 +0200

    Update hunter_wifi.cpp

--- components/hunter_wifi/hunter_wifi.cpp
+++ components/hunter_wifi/hunter_wifi.cpp
@@ -76,12 +76,14 @@ void HunterWifiComponent::configure_valve_switch(size_t valve_number, switch_::S
   }
 }
 
-void HunterWifiComponent::add_valve(HunterZoneSwitch  *valve_sw) {
+// add_valve(HunterZoneSwitch *valve_sw, uint16_t zone_number); 
+void HunterWifiComponent::add_valve(HunterZoneSwitch  *valve_sw, uint16_t zone_number) {
   auto new_valve_number = this->number_of_valves();
   this->valve_.resize(new_valve_number + 1);
   HunterValve *new_valve = &this->valve_[new_valve_number];
 
   new_valve->valve_switch = valve_sw;
+  new_valve->zone_number = zone_number;
 }
 
 void HunterWifiComponent::register_switch(HunterZoneSwitch  *switch) { this->switches_.push_back(switch); }
