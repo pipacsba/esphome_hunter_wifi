72b454c0167914b4d052567d72914dc967a42c02
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-04-06 14:11:35 +0200

    Update __init__.py

--- components/hunter_wifi/__init__.py
+++ components/hunter_wifi/__init__.py
@@ -100,7 +100,8 @@ async def to_code(config):
     for valve_index, valve in enumerate(hunterwifi_controller[CONF_VALVES]):
         sw_valve_var = await switch.new_switch(valve[CONF_ID])
         await cg.register_component(sw_valve_var, valve[CONF_ID])
-        cg.add(var.add_valve(sw_valve_var, valve[CONF_NUMBER]))
+        int zone_number = valve[CONF_NUMBER][CONF_NAME]
+        cg.add(var.add_valve(sw_valve_var, zone_number)
         
     pin = await cg.gpio_pin_expression(hunterwifi_controller[CONF_PIN])
     cg.add(var.set_pin(pin))
