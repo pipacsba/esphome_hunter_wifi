449888b34bde68c5a34af461580d71f6fa66eefa
pipacsba <44190358+pipacsba@users.noreply.github.com>
2024-02-28 19:25:00 +0100

    Update hunter_wifi.cpp

--- components/hunter_wifi/hunter_wifi.cpp
+++ components/hunter_wifi/hunter_wifi.cpp
@@ -146,7 +146,7 @@ size_t HunterWifiComponent::number_of_valves() { return this->valve_.size(); }
  * 
  * @param pin GPIO number where the REM wire is connected to.
  */
-void HunterRoam::HunterRoam(InternalGPIOPin *pin) {
+void HunterRoam::setup(InternalGPIOPin *pin) {
 	_pin = pin;
 	this->pin_->pin_mode(gpio::FLAG_OUTPUT);
 }
