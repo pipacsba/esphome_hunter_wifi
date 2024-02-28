51182785587822d75d3cd9eee5f2751042267e69
pipacsba <44190358+pipacsba@users.noreply.github.com>
2024-02-28 19:35:14 +0100

    Update hunter_wifi.h

--- components/hunter_wifi/hunter_wifi.h
+++ components/hunter_wifi/hunter_wifi.h
@@ -20,7 +20,7 @@ class HunterRoam;               // bus handling
 
 class HunterRoam : public Component {
   public:
-    void setup(InternalGPIOPin *pin);
+    HunterRoam(InternalGPIOPin *pin);
     uint8_t stopZone(uint8_t zone);
     uint8_t startZone(uint8_t zone, uint8_t time);
     uint8_t startProgram(uint8_t num);
